8c8d7496a4f9b9569b4b9ec2c4dc133c43639bf5
rajkumar <rajkumars@melss.com>
2025-01-25 15:34:10 +0530

    deleted

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="us_visa",
-    version="0.0.0",
-    author="rajkumar",
-    author_email="crajkumar97@gmail.com",
-    packages=find_packages()
-)
